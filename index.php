e4f0c6a41663ef72927e5a74a4856efce4b3a368
2207akash <2207akash@gmail.com>
2020-09-06 08:56:03 +0530

    Designed the search-engine logo, added basic HTML structure for the engine

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Searchbay</title>
+</head>
+<body>
+
+	<div class="mainContainer">
+		
+		<div class="logoContainer">
+			<img src="assets/images/logo.png">
+		</div>
+
+		<div class="searchContainer">
+			
+			<form action="search.php" method="GET">
+				<input class="searchBox" type="text" name="query">
+				<input class="searchButton" type="submit" value="Search">
+			</form>
+
+		</div>
+
+	</div>
+
+</body>
+</html>
